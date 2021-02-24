3dab9ab445612d2e0af083929ecfc472da11d2ec
DongMin-Lee <pkalsh345@gmail.com>
2021-02-25 02:51:23 +0900

    test: 빼기 테스트 케이스 작성

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,9 +6,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
 
+    private Calculator calculator = new Calculator();
+
     @Test
     void 더하기_테스트() {
-        Calculator calculator = new Calculator();
         int test1Result = calculator.calculate("1+1");
         assertThat(test1Result).isEqualTo(2);
 
@@ -17,5 +18,17 @@ public class CalculatorTest {
 
         int test3Result = calculator.calculate("10+234");
         assertThat(test3Result).isEqualTo(244);
+    }
+
+    @Test
+    void 빼기_테스트() {
+        int test1Result = calculator.calculate("1-1");
+        assertThat(test1Result).isEqualTo(0);
+
+        int test2Result = calculator.calculate("998-234");
+        assertThat(test2Result).isEqualTo(764);
+
+        int test3Result = calculator.calculate("10003-234");
+        assertThat(test3Result).isEqualTo(9769);
     }
 }
