71ae7ee56357cf79a1b41d58bc08ab4512f47b33
DongMin-Lee <pkalsh345@gmail.com>
2021-02-25 02:52:05 +0900

    feat: 빼기 테스트 통과하는 메소드 구현

--- src/main/java/domain/calculator/Calculator.java
+++ src/main/java/domain/calculator/Calculator.java
@@ -3,9 +3,13 @@ package domain.calculator;
 public class Calculator {
 
     public int calculate(String expression) {
-        String[] splitedExpression = expression.split("\\+");
+        String[] splitedExpression = expression.split("\\+|-");
         int left = Integer.parseInt(splitedExpression[0]);
         int right = Integer.parseInt(splitedExpression[1]);
+
+        if (expression.contains("-")) {
+            return left - right;
+        }
 
         return left + right;
     }
