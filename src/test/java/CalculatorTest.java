5c07d20131a8af0569f793d263291038e16c0af8
DongMin-Lee <pkalsh345@gmail.com>
2021-02-25 16:26:51 +0900

    test: 공백으로 구분하는 테스트케이스로 한정

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class CalculatorTest {
 
@@ -11,37 +10,37 @@ public class CalculatorTest {
 
     @Test
     void 더하기_테스트() {
-        int test1Result = calculator.calculate("1+1");
+        int test1Result = calculator.calculate("1 + 1");
         assertThat(test1Result).isEqualTo(2);
 
-        int test2Result = calculator.calculate("3+7");
+        int test2Result = calculator.calculate("3 + 7");
         assertThat(test2Result).isEqualTo(10);
 
-        int test3Result = calculator.calculate("10+234");
+        int test3Result = calculator.calculate("10 + 234");
         assertThat(test3Result).isEqualTo(244);
     }
 
     @Test
     void 빼기_테스트() {
-        int test1Result = calculator.calculate("1-1");
+        int test1Result = calculator.calculate("1 - 1");
         assertThat(test1Result).isEqualTo(0);
 
-        int test2Result = calculator.calculate("998-234");
+        int test2Result = calculator.calculate("998 - 234");
         assertThat(test2Result).isEqualTo(764);
 
-        int test3Result = calculator.calculate("10003-234");
+        int test3Result = calculator.calculate("10003 - 234");
         assertThat(test3Result).isEqualTo(9769);
     }
 
     @Test
     void 곱하기_테스트() {
-        int test1Result = calculator.calculate("2*34");
+        int test1Result = calculator.calculate("2 * 34");
         assertThat(test1Result).isEqualTo(68);
 
-        int test2Result = calculator.calculate("60*61");
+        int test2Result = calculator.calculate("60 * 61");
         assertThat(test2Result).isEqualTo(3660);
 
-        int test3Result = calculator.calculate("430*289");
+        int test3Result = calculator.calculate("430 * 289");
         assertThat(test3Result).isEqualTo(124270);
     }
 }
