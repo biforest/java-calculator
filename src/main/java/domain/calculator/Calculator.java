3bf7552f5a87005d2e7cea1a847511085ad2c756
DongMin-Lee <pkalsh345@gmail.com>
2021-02-25 12:49:05 +0900

    feat: 곱하기 테스트 통과하는 메소드 구현

--- src/main/java/domain/calculator/Calculator.java
+++ src/main/java/domain/calculator/Calculator.java
@@ -3,12 +3,14 @@ package domain.calculator;
 public class Calculator {
 
     public int calculate(String expression) {
-        String[] splitedExpression = expression.split("\\+|-");
+        String[] splitedExpression = expression.split("\\+|-|\\*|/");
         int left = Integer.parseInt(splitedExpression[0]);
         int right = Integer.parseInt(splitedExpression[1]);
 
         if (expression.contains("-")) {
             return left - right;
+        } else if (expression.contains("*")) {
+            return left * right;
         }
 
         return left + right;
