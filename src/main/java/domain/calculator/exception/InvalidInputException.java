202fddb8f30e8b558fb0b832f7f7c7066da5890c
DongMin-Lee <pkalsh345@gmail.com>
2021-03-21 16:39:12 +0900

    refactor: 예외 클래스 메시지 출력 기능 간소화
    getMessage 메소드는 이미 java.lang 상위 예외 클래스에 동일한 메소드 정의가 존재하므로 super 키워드로 간소화 가능

--- src/main/java/domain/calculator/exception/InvalidInputException.java
+++ src/main/java/domain/calculator/exception/InvalidInputException.java
@@ -2,14 +2,8 @@ package domain.calculator.exception;
 
 public class InvalidInputException extends RuntimeException {
 
-    private String message;
-
     public InvalidInputException(String message) {
-        this.message = message;
+        super(message);
     }
 
-    @Override
-    public String getMessage() {
-        return message;
-    }
 }
