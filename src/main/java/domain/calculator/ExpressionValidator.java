81c5f1a6fef29fff66e94aae795d4c075aa0bc6c
DongMin-Lee <pkalsh345@gmail.com>
2021-03-21 16:43:26 +0900

    refactor: 상수 정의하여 의미 부여

--- src/main/java/domain/calculator/ExpressionValidator.java
+++ src/main/java/domain/calculator/ExpressionValidator.java
@@ -7,6 +7,10 @@ import java.util.regex.Pattern;
 
 public class ExpressionValidator {
 
+    private static final int HEAD_TOKEN_INDEX = 1;
+    private static final int FIRST_OPERATOR_INDEX = 1;
+    private static final int NEXT_OPERATOR_STEP = 2;
+
     public void validateTokenList(List<String> tokenList) {
         long invalidTokenCount = tokenList.stream()
                 .filter(token -> !isNumber(token) && !isOperator(token))
@@ -20,10 +24,10 @@ public class ExpressionValidator {
     public void validateTokenSequence(List<String> tokenList) {
         throwIfConditionIsTrue(hasNotAnyToken(tokenList));
 
-        String firstToken = tokenList.get(0);
+        String firstToken = tokenList.get(HEAD_TOKEN_INDEX);
         throwIfConditionIsTrue(!isNumber(firstToken));
 
-        for (int i = 1; i < tokenList.size(); i += 2) {
+        for (int i = FIRST_OPERATOR_INDEX; i < tokenList.size(); i += NEXT_OPERATOR_STEP) {
             String operatorToken = tokenList.get(i);
             throwIfConditionIsTrue(!isOperator(operatorToken));
 
